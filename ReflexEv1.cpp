5adec361672e8b31eef0825627acdeddc0c416b4
María Fernanda García Bushbeck <177565556+bbushbeck@users.noreply.github.com>
2025-09-29 21:13:52 -0600

    Update ReflexEv1.cpp

--- ReflexEv1.cpp
+++ ReflexEv1.cpp
@@ -1,3 +1,8 @@
+/* César Tadeo Bernal Saucedo
+Regina Aguilar García
+María Fernanda García Bushbeck
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -5,9 +10,13 @@
 using namespace std;
 
 const int MAX = 1000000;
+
+// Arreglos globales
 string meses[MAX], horas[MAX], ips[MAX], puertos[MAX], razones[MAX];
 int dias[MAX];
+int mesInt[MAX], horasInt[MAX]; 
 
+// Convertir datos a enteros
 int mesAEntero(string mes) {
     if (mes == "Jan") return 1; if (mes == "Feb") return 2;
     if (mes == "Mar") return 3; if (mes == "Apr") return 4;
@@ -18,13 +27,15 @@ int mesAEntero(string mes) {
     return 0;
 }
 
-int horaAEntero(string h) {
+ // Convertir horas a segundos y a enteros para más facilidad
+    int horaAEntero(string h) {
     int hh = stoi(h.substr(0,2));
     int mm = stoi(h.substr(3,2));
     int ss = stoi(h.substr(6,2));
     return hh*3600 + mm*60 + ss;
 }
 
+// Método para leer archivo
 int leerArchivo(string nombreArchivo) {
     ifstream archivo(nombreArchivo);
     if (!archivo.is_open()) {
@@ -34,14 +45,22 @@ int leerArchivo(string nombreArchivo) {
 
     int contador = 0;
     string linea;
-    while (contador < MAX && archivo >> meses[contador] >> dias[contador] >> horas[contador] >> ips[contador]) {
+    while (contador < MAX && (archivo >> meses[contador] >> dias[contador] >> horas[contador] >> ips[contador])) {
         // Separar puerto de IP
         int pos = ips[contador].find(':');
-        string ip_solo = ips[contador].substr(0, pos);
-        puertos[contador] = ips[contador].substr(pos + 1);
+        if (pos != string::npos) {
+            puertos[contador] = ips[contador].substr(pos + 1);
+            ips[contador] = ips[contador].substr(0, pos);
+        } else {
+            puertos[contador] = "";
+        }
 
         // Leer el resto de la línea como razón
         getline(archivo, razones[contador]);
+
+        // Llenar auxiliares
+        mesInt[contador]   = mesAEntero(meses[contador]);
+        horasInt[contador] = horaAEntero(horas[contador]);
 
         contador++;
     }
@@ -50,21 +69,63 @@ int leerArchivo(string nombreArchivo) {
     return contador; // cantidad de registros leídos
 }
 
+// Comparar los datos por índice
+inline bool esMenorIdx(int i, int j){
+    if (mesInt[i] != mesInt[j]) return mesInt[i] < mesInt[j];
+    if (dias[i]   != dias[j])   return dias[i]   < dias[j];
+    return horasInt[i] < horasInt[j];
+}
 
-int main() {
+void mergeIdx(int idx[], int temp[], int l, int m, int r){ 
+    int i = l, j = m + 1, k = l;
+    while (i <= m && j <= r) {
+        if (esMenorIdx(idx[i], idx[j])) {
+            temp[k++] = idx[i++];
+        } else {
+            temp[k++] = idx[j++];
+        }
+    }
+    while (i <= m) {
+        temp[k++] = idx[i++];
+    }
+    while (j <= r) {
+        temp[k++] = idx[j++];
+    }
+    for (int t = l; t <= r; ++t) {
+        idx[t] = temp[t];
+    }
+}
+
+// Merge sort recursivo
+void mergeSortIdxRec(int idx[], int temp[], int l, int r) {
+    if (l >= r) return;
+    int m = l + (r - l)/2;
+    mergeSortIdxRec(idx, temp, l, m);
+    mergeSortIdxRec(idx, temp, m+1, r);
+    mergeIdx(idx, temp, l, m, r);
+}
+
+void ordenar(int idx[], int n) {
+    static int temp[MAX];           
+    mergeSortIdxRec(idx, temp, 0, n-1);
+}
+
+int main(){
     int n = leerArchivo("bitacora.txt");
 
-    // Convertir meses a enteros
-    int mesInt[MAX];
-    for (int i = 0; i < n; i++) {
-        mesInt[i] = mesAEntero(meses[i]);
-    }
+    int idx[MAX];
+    for (int i = 0; i < n; ++i) idx[i] = i;
 
-    // Convertir horas a segundos y a enteros para más facilidad
-    int horasInt[MAX];
-    for (int i = 0; i < n; i++) {
-        horasInt[i] = horaAEntero(horas[i]);
-    }
-
+    ordenar(idx, n);
+    
+    // Impresión del ordenamiento
+    for (int k = 0; k < n; ++k) {
+    int i = idx[k]; 
+    cout << meses[i] << " " 
+         << dias[i] << " " 
+         << horas[i] << " "
+         << ips[i] << ":" << puertos[i] << " "
+         << razones[i] << "\n";
+}
     return 0;
 }
